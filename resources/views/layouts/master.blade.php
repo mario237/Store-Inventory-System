89417ba243056ec1bbda858732302eb7cda0ebe2
mario237 <mariomamdouh237@gmail.com>
2021-12-01 01:09:18 +0200

    init commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+@include('layouts.partials._head')
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+    <!-- Preloader -->
+    <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="animation__shake" src="{{ asset('admin/dist/img/online-shop.png') }}" alt="AdminLTELogo" height="60" width="60">
+    </div>
+
+    @include('layouts.partials._navbar')
+    @include('layouts.partials._sidebar')
+
+
+
+    <div class="content-wrapper">
+
+        @yield('content')
+
+    </div>
+
+
+
+
+</div>
+
+
+
+
+@include('layouts.partials._footer-script')
+
+</body>
+</html>
---
+++ resources/views/layouts/partials/_head.blade.php
@@ -0,0 +1,29 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>{{ config('app.name') }}</title>
+
+<link rel="icon" type="image/x-icon" href="{{ asset('admin/dist/img/online-shop.png') }}">
+
+
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{ asset('admin/plugins/fontawesome-free/css/all.min.css') }}">
+
+<!-- Tempusdominus Bootstrap 4 -->
+<link rel="stylesheet"
+      href="{{ asset('admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+<!-- iCheck -->
+<link rel="stylesheet" href="{{ asset('admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+<!-- JQVMap -->
+<link rel="stylesheet" href="{{ asset('admin/plugins/jqvmap/jqvmap.min.css') }}">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{ asset('admin/dist/css/adminlte.min.css') }}">
+<!-- overlayScrollbars -->
+<link rel="stylesheet" href="{{ asset('admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+<!-- Daterange picker -->
+<link rel="stylesheet" href="{{ asset('admin/plugins/daterangepicker/daterangepicker.css') }}">
+<!-- summernote -->
+<link rel="stylesheet" href="{{ asset('admin/plugins/summernote/summernote-bs4.min.css') }}">
+
+@stack('css')
